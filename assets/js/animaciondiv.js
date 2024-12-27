83e66d9f5230c7d812128d5c5e70ddddc05686ba
juansotelo1709 <juan.david.jds84@gmail.com>
2024-12-26 19:08:40 -0500

    Resolvió

--- assets/js/animaciondiv.js
+++ assets/js/animaciondiv.js
@@ -1,3 +1,20 @@
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("in-view");
+            }
+        });
+    }, { threshold: 0.1 }); // Puedes ajustar el umbral según sea necesario
+
+    const elements = document.querySelectorAll(".about_me-container");
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     window.addEventListener('scroll', function() {
         const elements = document.querySelectorAll('.animate__animated');

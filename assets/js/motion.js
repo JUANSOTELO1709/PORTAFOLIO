2760fe5d7e9a9d81438f70ab57a0e6d565fde9a9
juansotelo1709 <juan.david.jds84@gmail.com>
2024-12-26 19:44:53 -0500

    actualizacion

--- assets/js/motion.js
+++
@@ -1,8 +0,0 @@
-import { animate } from "motion";
-
-document.addEventListener("DOMContentLoaded", function() {
-    const title = document.getElementById("title-juan-sotelo");
-
-    // Aplicar la animación al título
-    animate(title, { scale: [0.8, 1.1] }, { ease: "circInOut", duration: 1 });
-});

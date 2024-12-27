02f12755aaec4f59e62de775944440d97ec7454e
juansotelo1709 <juan.david.jds84@gmail.com>
2024-12-26 19:54:34 -0500

    actualizacion1

--- assets/js/animaciondiv.js
+++ assets/js/animaciondiv.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
                 entry.target.classList.add("in-view");
             }
         });
-    }, { threshold: 0.1 }); // Puedes ajustar el umbral según sea necesario
+    }, { threshold: 0.2 }); // Puedes ajustar el umbral según sea necesario
 
     const elements = document.querySelectorAll(".about_me-container");
     elements.forEach(element => {
@@ -27,5 +27,25 @@ document.addEventListener("DOMContentLoaded", function() {
                 element.classList.add('scroll-in');
             }
         });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const logos = document.querySelectorAll(".skill-logo");
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                const logo = entry.target;
+                setTimeout(() => {
+                    logo.classList.add("in-view");
+                }, Array.from(logos).indexOf(logo) * 200); // Ajusta el intervalo de tiempo entre cada animación
+                observer.unobserve(logo); // Dejar de observar una vez que la animación se haya activado
+            }
+        });
+    }, { threshold: 0.1 });
+
+    logos.forEach((logo) => {
+        observer.observe(logo);
     });
 });

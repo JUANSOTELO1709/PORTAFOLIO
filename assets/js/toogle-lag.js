24f6093b0ca83d6e1f67c5f5586f252e38786b73
juansotelo1709 <juan.david.jds84@gmail.com>
2025-01-25 15:04:43 -0500

    actualiza perfil-english0.1

--- assets/js/toogle-lag.js
+++ assets/js/toogle-lag.js
@@ -1,19 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const toggleLangButton = document.getElementById('toggle-lang');
-    const spanishHeader = document.getElementById('spanish-header');
-    const englishHeader = document.getElementById('english-header');
+    const toggleLangButton = document.getElementById('toggle-lang-about_me');
     const spanishAboutMe = document.getElementById('spanish-about_me');
     const englishAboutMe = document.getElementById('english-about_me');
 
     toggleLangButton.addEventListener('click', () => {
-        if (spanishHeader.style.display === 'none') {
-            spanishHeader.style.display = 'block';
-            englishHeader.style.display = 'none';
+        if (spanishAboutMe.style.display === 'none') {
             spanishAboutMe.style.display = 'block';
             englishAboutMe.style.display = 'none';
         } else {
-            spanishHeader.style.display = 'none';
-            englishHeader.style.display = 'block';
             spanishAboutMe.style.display = 'none';
             englishAboutMe.style.display = 'block';
         }
